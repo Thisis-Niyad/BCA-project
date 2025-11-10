361099aebe4c8bfc66830e4d4c6c1c39039a60ec
MUHAMMED NIYAD <muhammedniyad720@gmail.com>
2025-11-10 21:28:46 +0530

    template setup

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,5 +1,5 @@
 // import { useState } from 'react'
-
+import Navbar from '../components/Navbar'
 
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-     hello
+     <Navbar/>
     </>
   )
 }
